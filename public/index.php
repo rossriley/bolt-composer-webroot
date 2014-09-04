df383a103b0a84a6274a812ccaf8382664585779
Ross Riley <riley.ross@gmail.com>
2014-09-04 21:05:49 +0100

    setup public dir for web

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once "vendor/autoload.php";
-$configuration = new Bolt\Configuration\Composer(__DIR__);
+require_once "../vendor/autoload.php";
+$configuration = new Bolt\Configuration\Composer(dirname(__DIR__));
 $configuration->compat();
 $configuration->getVerifier()->removeCheck('apache');
 $configuration->verify();
